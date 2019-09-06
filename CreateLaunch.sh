c53fb4f9a60b9cdd2bd9dfe1cf70dc5fe97eb87e
Indiorlei de Oliveira <indiorleioliveira@gmail.com>
2019-09-06 14:14:58 -0300

    Update CreateLaunch.sh

--- CreateLaunch.sh
+++ CreateLaunch.sh
@@ -92,3 +92,87 @@ sips -Z 2008 -c 1536 2008 launch.png --out Images.xcassets/LaunchImage.launchima
 # 2x	iOS 5,6	Landscape (Without Status Bar)	2048 x 1496
 sips -Z 2048 -c 2048 1496 launch.png --out Images.xcassets/LaunchImage.launchimage/iPad-2x-iOS-5-6-Landscape-Without-Status-Bar-2048x1496-Portrait.png
 sips -Z 2048 -c 1496 2048 launch.png --out Images.xcassets/LaunchImage.launchimage/iPad-2x-iOS-5-6-Landscape-Without-Status-Bar-2048x1496-Landscape.png
+
+# 320 × 480
+sips -Z 480 -c 320 480 launch.png --out Images.xcassets/LaunchImage.launchimage/320x480-Portrait.png
+sips -Z 480 -c 480 320 launch.png --out Images.xcassets/LaunchImage.launchimage/320x480-Landscape.png
+
+# 640 × 960
+sips -Z 960 -c 640 960 launch.png --out Images.xcassets/LaunchImage.launchimage/640x960-Portrait.png
+sips -Z 960 -c 960 640 launch.png --out Images.xcassets/LaunchImage.launchimage/640x960-Landscape.png
+
+# 640 × 960
+sips -Z 960 -c 640 960 launch.png --out Images.xcassets/LaunchImage.launchimage/640x960-Portrait.png
+sips -Z 960 -c 960 640 launch.png --out Images.xcassets/LaunchImage.launchimage/640x960-Landscape.png
+
+# 640 × 1136
+sips -Z 1136 -c 640 1136 launch.png --out Images.xcassets/LaunchImage.launchimage/640x1136-Portrait.png
+sips -Z 1136 -c 1136 640 launch.png --out Images.xcassets/LaunchImage.launchimage/640x1136-Landscape.png
+
+# 750 × 1334
+sips -Z 1334 -c 750 1334 launch.png --out Images.xcassets/LaunchImage.launchimage/750x1334-Portrait.png
+sips -Z 1334 -c 1334 750 launch.png --out Images.xcassets/LaunchImage.launchimage/750x1334-Landscape.png
+
+# 768 × 1004
+sips -Z 1004 -c 768 1004 launch.png --out Images.xcassets/LaunchImage.launchimage/768x1004-Portrait.png
+sips -Z 1004 -c 1004 768 launch.png --out Images.xcassets/LaunchImage.launchimage/768x1004-Landscape.png
+
+# 768 × 1024
+sips -Z 1024 -c 768 1024 launch.png --out Images.xcassets/LaunchImage.launchimage/768x1024-Portrait.png
+sips -Z 1024 -c 1024 768 launch.png --out Images.xcassets/LaunchImage.launchimage/768x1024-Landscape.png
+
+# 828 × 1792
+sips -Z 1792 -c 828 1792 launch.png --out Images.xcassets/LaunchImage.launchimage/828x1792-Portrait.png
+sips -Z 1792 -c 1792 828 launch.png --out Images.xcassets/LaunchImage.launchimage/828x1792-Landscape.png
+
+# 1024 × 748
+sips -Z 1024 -c 1024 748 launch.png --out Images.xcassets/LaunchImage.launchimage/1024x748-Portrait.png
+sips -Z 1024 -c 748 1024 launch.png --out Images.xcassets/LaunchImage.launchimage/1024x748-Landscape.png
+
+# 1024 × 768
+sips -Z 1024 -c 1024 768 launch.png --out Images.xcassets/LaunchImage.launchimage/1024x768-Portrait.png
+sips -Z 1024 -c 768 1024 launch.png --out Images.xcassets/LaunchImage.launchimage/1024x768-Landscape.png
+
+# 1125 × 2436
+sips -Z 2436 -c 1125 2436 launch.png --out Images.xcassets/LaunchImage.launchimage/1125x2436-Portrait.png
+sips -Z 2436 -c 2436 1125 launch.png --out Images.xcassets/LaunchImage.launchimage/1125x2436-Landscape.png
+
+# 1242 × 2208
+sips -Z 2208 -c 1242 2208 launch.png --out Images.xcassets/LaunchImage.launchimage/1242x2208-Portrait.png
+sips -Z 2208 -c 2208 1242 launch.png --out Images.xcassets/LaunchImage.launchimage/1242x2208-Landscape.png
+
+# 1242 × 2688
+sips -Z 2688 -c 1242 2688 launch.png --out Images.xcassets/LaunchImage.launchimage/1242x2688-Portrait.png
+sips -Z 2688 -c 2688 1242 launch.png --out Images.xcassets/LaunchImage.launchimage/1242x2688-Landscape.png
+
+# 1536 × 2008
+sips -Z 2008 -c 1536 2008 launch.png --out Images.xcassets/LaunchImage.launchimage/1536x2008-Portrait.png
+sips -Z 2008 -c 2008 1536 launch.png --out Images.xcassets/LaunchImage.launchimage/1536x2008-Landscape.png
+
+# 1536 × 2048
+sips -Z 2048 -c 1536 2048 launch.png --out Images.xcassets/LaunchImage.launchimage/1536x2048-Portrait.png
+sips -Z 2048 -c 2048 1536 launch.png --out Images.xcassets/LaunchImage.launchimage/1536x2048-Landscape.png
+
+# 1792 × 828
+sips -Z 1792 -c 1792 828 launch.png --out Images.xcassets/LaunchImage.launchimage/1792x828-Portrait.png
+sips -Z 1792 -c 828 1792 launch.png --out Images.xcassets/LaunchImage.launchimage/1792x828-Landscape.png
+
+# 2048 × 1496
+sips -Z 2048 -c 2048 1496 launch.png --out Images.xcassets/LaunchImage.launchimage/2048x1496-Portrait.png
+sips -Z 2048 -c 1496 2048 launch.png --out Images.xcassets/LaunchImage.launchimage/2048x1496-Landscape.png
+
+# 2048 × 1536
+sips -Z 2048 -c 2048 1536 launch.png --out Images.xcassets/LaunchImage.launchimage/2048x1536-Portrait.png
+sips -Z 2048 -c 1536 2048 launch.png --out Images.xcassets/LaunchImage.launchimage/2048x1536-Landscape.png
+
+# 2208 × 1242
+sips -Z 2208 -c 2048 1242 launch.png --out Images.xcassets/LaunchImage.launchimage/2048x1242-Portrait.png
+sips -Z 2208 -c 1242 2048 launch.png --out Images.xcassets/LaunchImage.launchimage/2048x1242-Landscape.png
+
+# 2436 × 1125
+sips -Z 2436 -c 2436 1125 launch.png --out Images.xcassets/LaunchImage.launchimage/2436x1125-Portrait.png
+sips -Z 2436 -c 1125 2436 launch.png --out Images.xcassets/LaunchImage.launchimage/2436x1125-Landscape.png
+
+# 2688 × 1242
+sips -Z 2048 -c 2048 1242 launch.png --out Images.xcassets/LaunchImage.launchimage/2048x1242-Portrait.png
+sips -Z 2048 -c 1242 2048 launch.png --out Images.xcassets/LaunchImage.launchimage/2048x1242-Landscape.png
